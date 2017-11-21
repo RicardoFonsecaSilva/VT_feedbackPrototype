fdcdcce57955310d0d7364038e06ecbceb8be6c7
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-21 18:27:12 +0000

    Maria Animatior

---
+++ Assets/randomIdleIndex.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomIdleIndex : StateMachineBehaviour
+{
+
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+
+        animator.SetInteger("IdleIndex", Random.Range(0, 2));
+
+    }
+}
