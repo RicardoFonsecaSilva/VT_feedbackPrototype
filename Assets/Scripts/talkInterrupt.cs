8be1c92862351d90e6311bd2370dc6c9c66d2566
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-23 18:06:38 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/talkInterrupt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talkInterrupt : StateMachineBehaviour {
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if (animator.GetBool("beingTalkedTo"))
+		{
+			Debug.Log("CROSSFADING");
+			animator.CrossFade("m_gaze_left", 0.3f);
+		}
+
+	}
+}
