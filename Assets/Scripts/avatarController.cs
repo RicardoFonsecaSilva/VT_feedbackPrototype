5d49167ecc577b7d2ea5253212566dcc191c965d
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-25 15:02:49 +0000

    Expression On Demand

--- Assets/Scripts/avatarController.cs
+++ Assets/Scripts/avatarController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,29 +9,7 @@ public class avatarController : MonoBehaviour
     public GameObject femaleAvatar;
     private Animator maleAnimator;
     private Animator femaleAnimator;
-
-    void OnEnable()
-    {
-        hookController.OnTalkRequest += HandleTalkRequest;
-        hookController.OnMoodChange += HandleMoodChange;
-    }
-    void OnDisable()
-    {
-        hookController.OnTalkRequest -= HandleTalkRequest;
-        hookController.OnMoodChange -= HandleMoodChange;
-    }
-
-    void HandleTalkRequest(string source, int param1)
-    {
-        Debug.Log(gameObject.name + ": \"Talk\" event was received." + source);
-        zoomIn(source);
-        turnHead(source);
-    }
-    void HandleMoodChange(string source, int param1)
-    {
-        Debug.Log(gameObject.name + ": \"Mood\" event was received." + source + "; " + param1);
-        updateMood(source, param1);
-    }
+    private string expression = "Null";
 
     void Start()
     {
@@ -38,17 +17,48 @@ public class avatarController : MonoBehaviour
         femaleAnimator = femaleAvatar.GetComponent<Animator>();
     }
 
-    private void updateMood(string avatar, int mood)
+    void OnEnable()
     {
-        if (avatar == "Joao")
+        hookController.OnTalkRequest += HandleTalkRequest;
+        hookController.OnMoodChange += HandleMoodChange;
+        hookController.OnExpressionRequest += HandleExpressionRequest;
+    }
+
+    void OnDisable()
+    {
+        hookController.OnTalkRequest -= HandleTalkRequest;
+        hookController.OnMoodChange -= HandleMoodChange;
+        hookController.OnExpressionRequest -= HandleExpressionRequest;
+    }
+
+    private void HandleTalkRequest(string source, int param1)
+    {
+        Debug.Log(gameObject.name + ": \"Talk\" event was received." + source);
+        zoomIn(source);
+        turnHead(source);
+    }
+    private void HandleMoodChange(string source, int param1)
+    {
+        Debug.Log(gameObject.name + ": \"Mood\" event was received." + source + "; " + param1);
+        updateMood(source, param1);
+    }
+    private void HandleExpressionRequest(string source, int param)
+    {
+        Debug.Log(gameObject.name + ": \"Expression\" event was received." + source + "; " + param);
+        expressEmotion(source, param);
+    }
+
+    private void updateMood(string source, int param)
+    {
+        if (source == "Joao")
         {
-            maleAnimator.SetInteger("CurrentMood", mood);
+            maleAnimator.SetInteger("CurrentMood", param);
             return;
         }
             
-        if (avatar == "Maria")
+        if (source == "Maria")
         {
-            femaleAnimator.SetInteger("CurrentMood", mood);
+            femaleAnimator.SetInteger("CurrentMood", param);
             return;
         }
         Debug.Log("ERROR: COULD NOT FIND CORRECT AVATAR.");
@@ -82,5 +92,60 @@ public class avatarController : MonoBehaviour
             return;
         }
         Debug.Log("ERROR: COULD NOT FIND THE SPEECH SOURCE.");
+    }
+    private void expressEmotion(string source, int param)
+    {
+        switch (param)
+        {
+            case 1:
+                expression = "Expression - Fear";
+                break;
+            case 2:
+                expression = "Expression - Fear (Intense)";
+                break;
+            case 3:
+                expression = "Expression - Rage";
+                break;
+            case 4:
+                expression = "Expression - Rage (Intense)";
+                break;
+            case 5:
+                expression = "Expression - Disgust";
+                break;
+            case 6:
+                expression = "Expression - Disgust (Intense)";
+                break;
+            case 7:
+                expression = "Expression - Surprise";
+                break;
+            case 8:
+                expression = "Expression - Surprise (Intense)";
+                break;
+            case 9:
+                expression = "Expression - Sadness";
+                break;
+            case 10:
+                expression = "Expression - Sadness (Intense)";
+                break;
+            case 11:
+                expression = "Expression - Happiness";
+                break;
+            case 12:
+                expression = "Expression - Happiness (Intense)";
+                break;
+            default:
+                break;
+        }
+
+        if (source == "Joao")
+        {
+            maleAnimator.CrossFade(expression, 0.3F, maleAnimator.GetLayerIndex("Expression"));
+            return;
+        }
+        if (source == "Maria")
+        {
+            femaleAnimator.CrossFade(expression, 0.3F, femaleAnimator.GetLayerIndex("Expression"));
+            return;
+        }
     }
 }
