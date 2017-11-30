5f2746a7bc0b6c1f38771dfcb82647236ebd0a4f
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-30 12:43:28 +0000

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/talkInterrupt.cs
+++ Assets/Scripts/talkInterrupt.cs
@@ -8,16 +8,20 @@ public class talkInterrupt : StateMachineBehaviour {
 	{
 		animator.SetBool("isDoneTalking", false);
 	}
+
 	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		if (animator.GetBool("beingTalkedTo"))
 		{
 			//Debug.Log("Crossfading to \"Look At\"");
 			animator.CrossFade("Look At", 0.3f);
+			animator.SetBool("beingTalkedTo", false);
 		}
 		if (animator.GetBool("isTalking"))
 		{
 			//Debug.Log("Crossfading to \"Talk\"");
+			animator.SetBool("zoomAvatar", true);
 			animator.CrossFade("Talk", 0.3f);
+			animator.SetBool("isTalking", false);
 		}
 	}
 
