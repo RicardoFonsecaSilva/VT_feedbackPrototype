aaba5b903c523487480ca1f87229748ee674320f
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-30 19:42:49 +0000

    Auto stash before merge of "master" and "origin/master"
    Orbit camera
    Always facing characters

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+	public Transform camera;
+    public float tilt;
+    private Transform transform;
+
+	void Start()
+	{
+		transform = GetComponent<Transform>();
+	}
+
+	void LateUpdate ()
+	{
+		Vector3 v = camera.transform.position - transform.position;
+		transform.rotation = Quaternion.LookRotation(v) * Quaternion.Euler(tilt, 0, 0);
+	}
+}
