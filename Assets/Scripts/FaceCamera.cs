f71f9250cd290c0030d9b25d6ba7e4d4e5614ef0
Unknown <ricardo.filipe.silva@hotmail.com>
2017-12-02 03:13:58 +0000

    What a mess

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FaceCamera : MonoBehaviour {
+public class FaceCamera : MonoBehaviour
+{
 
-	public Transform camera;
+    public Transform camera;
     public float xTilt = 15.0f;
-    public float yAngle = 20.0f;
+    public float yTilt = 25.0f;
+    public float zTilt = 7.5f;
+    public Orientation orientation = Orientation.Right;
+
     private Transform transform;
 
-	void Start()
-	{
-		transform = GetComponent<Transform>();
+    void Start()
+    {
+        transform = GetComponent<Transform>();
     }
 
-	void LateUpdate ()
-	{
-		Vector3 v = camera.transform.position - transform.position;
-		transform.rotation = Quaternion.LookRotation(v) * Quaternion.Euler(xTilt, yAngle, 0);
+    void LateUpdate()
+    {
+        Vector3 v = camera.transform.position - transform.position;
+        float ori = (orientation == Orientation.Right ? 1.0f : -1.0f);
+        transform.rotation = Quaternion.LookRotation(v) * Quaternion.Euler(xTilt, yTilt * ori, zTilt * ori);
     }
+}
+
+public enum Orientation
+{
+    Left, Right
 }
