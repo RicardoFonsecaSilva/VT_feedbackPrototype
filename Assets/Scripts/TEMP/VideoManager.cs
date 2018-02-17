2dcc1964010b1726be4e7e3ff07586bc221fc84f
Ricardo <ricardo.g.pereira@ist.utl.pt>
2018-02-17 18:21:22 +0000

    script1 added

--- Assets/Scripts/TEMP/VideoManager.cs
+++ Assets/Scripts/TEMP/VideoManager.cs
@@ -18,20 +18,65 @@ public class VideoManager : MonoBehaviour {
         float t = 2.0f;
         balloonManager.text = "Hello.";
         balloonManager.timeToWait = t;
-        balloonManager.Generate(j);
-        avatarScriptHelper.talkFor(j, 1);
+        balloonManager.Generate(m);
+        avatarScriptHelper.talkFor(m, 1);
 
         // WE ALSO NEED TO WAIT FOR X SECONDS, CAUSE THIS SCRIPT KEEPS GOING
-        yield return new WaitForSeconds(t+1);
+        yield return new WaitForSeconds(2);
         //
 
-        balloonManager.MaleMoodChange(MoodState.HAPPY_HIGH);
-        balloonManager.FemaleMoodChange(MoodState.HAPPY_HIGH);
-        balloonManager.optionsText_1 = "Hello.";
-        balloonManager.optionsText_2 = "Hello.";
+        t = 2.0f;
+        balloonManager.text = "Good day.";
+        balloonManager.timeToWait = t;
+        balloonManager.Generate(j);
+        avatarScriptHelper.talkFor(j, 1.5f);
+
+        yield return new WaitForSeconds(3f);
+
+        //TODO
+        //M Look J
+
+        t = 5.0f;
+        balloonManager.text = "We wanted to inform you that a new checkpoint is out for the Introdução à Programação.";
+        balloonManager.timeToWait = t;
+        balloonManager.Generate(j);
+        avatarScriptHelper.talkFor(j, 3.5f);
+
+        yield return new WaitForSeconds(5.5f);
+
+        t = 6.0f;
+        balloonManager.text = "We see that your performance in this class has been very good lately.";
+        balloonManager.timeToWait = t;
+        balloonManager.Generate(j);
+        avatarScriptHelper.talkFor(j, 4f);
+
+        yield return new WaitForSeconds(6.5f);
+
+        balloonManager.FemaleMoodChange(MoodState.HAPPY_LOW);
+
+        t = 5.0f;
+        balloonManager.text = "You have a lot of completed assignments. Well done.";
+        balloonManager.timeToWait = t;
+        balloonManager.Generate(m);
+        avatarScriptHelper.talkFor(m, 3.5f);
+
+        yield return new WaitForSeconds(5.5f);
+
+        t = 8.0f;
+        balloonManager.text = "To continue the rhythm, I would ask you to please mark this new checkpoint in your calendar.";
+        balloonManager.timeToWait = t;
+        balloonManager.Generate(j);
+        avatarScriptHelper.talkFor(j, 5f);
+
+        yield return new WaitForSeconds(6f);
+
+
+        balloonManager.optionsText_1 = "Mark checkpoint.";
+        balloonManager.optionsText_2 = "Don't mark checkpoint.";
         balloonManager.timeToWait = 5.0f;
         balloonManager.Generate("Options");
-        avatarScriptHelper.express(m, 8);
+
+        //avatarScriptHelper.express(m, 8);
 
         yield return new WaitForSeconds(5);
         balloonManager.Clean(0.0f);
